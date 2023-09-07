f8a864ce34b312855a9610dd957aced0df2ad1e6
Patrick Gimenes <48632980+PatrickGimenes@users.noreply.github.com>
2023-09-07 02:26:34 -0300

    feat: remove poi module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 
 import { AppService } from './app.service';
 import { PointsModule } from './points/points.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [PointsModule],
--- src/poi/poi.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PoiService } from './poi.service';
-import { PoiController } from './poi.controller';
-
-@Module({
-  controllers: [PoiController],
-  providers: [PoiService],
-})
-export class PoiModule {}
